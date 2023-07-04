0f3005ebc2a5af5420e4a30e7cc8fecf9681f0a9
Nhat Doan <nhatdoan@Nhats-MacBook-Pro.local>
2023-07-04 14:34:05 +0700

    deploy

---
+++ dropdown.js
@@ -0,0 +1,4 @@
+function toggleDropdown() {
+    var dropdownImage = document.getElementById("dropdown-image");
+    dropdownImage.style.display = (dropdownImage.style.display === "none") ? "block" : "none";
+}
